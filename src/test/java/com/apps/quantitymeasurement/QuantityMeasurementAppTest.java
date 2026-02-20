9f74f85427ddf78622164e7565720773e38780dd
anjaligitlab27 <anjalikumari270704@gmail.com>
2026-02-20 15:48:54 +0530

    [Anjali] : Added Test file for feature/UC1-FeetEquality

---
+++ src/test/java/com/apps/quantitymeasurement/QuantityMeasurementAppTest.java
@@ -0,0 +1,45 @@
+package com.apps.quantitymeasurement;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class QuantityMeasurementAppTest {
+
+    @Test
+    void testEquality_SameValue() {
+        QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(1.0);
+        QuantityMeasurementApp.Feet f2 = new QuantityMeasurementApp.Feet(1.0);
+
+        assertTrue(f1.equals(f2), "1.0 ft should be equal to 1.0 ft");
+    }
+
+    @Test
+    void testEquality_DifferentValue() {
+        QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(1.0);
+        QuantityMeasurementApp.Feet f2 = new QuantityMeasurementApp.Feet(2.0);
+
+        assertFalse(f1.equals(f2), "1.0 ft should NOT be equal to 2.0 ft");
+    }
+
+    @Test
+    void testEquality_NullComparison() {
+        QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(1.0);
+
+        assertFalse(f1.equals(null), "1.0 ft should NOT be equal to null");
+    }
+
+    @Test
+    void testEquality_NonNumericInput() {
+        QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(1.0);
+
+        // equals takes Object, so "non-numeric input" means comparing with a non-Feet object
+        assertFalse(f1.equals("abc"), "Feet should NOT be equal to a non-Feet object");
+    }
+
+    @Test
+    void testEquality_SameReference() {
+        QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(1.0);
+
+        assertTrue(f1.equals(f1), "Object should be equal to itself (reflexive)");
+    }
+}
