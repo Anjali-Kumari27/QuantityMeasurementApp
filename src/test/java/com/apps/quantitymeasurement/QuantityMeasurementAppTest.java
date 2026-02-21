a939224f2099fe75196785452deefcfb731fed3f
anjaligitlab27 <anjalikumari270704@gmail.com>
2026-02-21 15:29:55 +0530

    [Anjali] : Added modified file of QuantityMeasurementAppTest.java

--- src/test/java/com/apps/quantitymeasurement/QuantityMeasurementAppTest.java
+++ src/test/java/com/apps/quantitymeasurement/QuantityMeasurementAppTest.java
@@ -5,41 +5,44 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class QuantityMeasurementAppTest {
 
-    @Test
-    void testEquality_SameValue() {
-        QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(1.0);
-        QuantityMeasurementApp.Feet f2 = new QuantityMeasurementApp.Feet(1.0);
+	// ---------------- FEET TESTS -------------------
+	@Test
+	void testEquality_SameValue() {
+		Feet f1 = new Feet(1.0);
+		Feet f2 = new Feet(1.0);
 
-        assertTrue(f1.equals(f2), "1.0 ft should be equal to 1.0 ft");
-    }
+		assertTrue(f1.equals(f2), "1.0 ft should be equal to 1.0 ft");
+	}
 
-    @Test
-    void testEquality_DifferentValue() {
-        QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(1.0);
-        QuantityMeasurementApp.Feet f2 = new QuantityMeasurementApp.Feet(2.0);
+	@Test
+	void testEquality_DifferentValue() {
+		Feet f1 = new Feet(1.0);
+		Feet f2 = new Feet(2.0);
 
-        assertFalse(f1.equals(f2), "1.0 ft should NOT be equal to 2.0 ft");
-    }
+		assertFalse(f1.equals(f2), "1.0 ft should NOT be equal to 2.0 ft");
+	}
 
-    @Test
-    void testEquality_NullComparison() {
-        QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(1.0);
+	@Test
+	void testEquality_NullComparison() {
+		Feet f1 = new Feet(1.0);
 
-        assertFalse(f1.equals(null), "1.0 ft should NOT be equal to null");
-    }
+		assertFalse(f1.equals(null), "1.0 ft should NOT be equal to null");
+	}
 
-    @Test
-    void testEquality_NonNumericInput() {
-        QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(1.0);
+	@Test
+	void testEquality_NonNumericInput() {
+		Feet f1 = new Feet(1.0);
 
-        // equals takes Object, so "non-numeric input" means comparing with a non-Feet object
-        assertFalse(f1.equals("abc"), "Feet should NOT be equal to a non-Feet object");
-    }
+		// equals takes Object, so "non-numeric input" means comparing with a non-Feet
+		// object
+		assertFalse(f1.equals("abc"), "Feet should NOT be equal to a non-Feet object");
+	}
 
-    @Test
-    void testEquality_SameReference() {
-        QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(1.0);
+	@Test
+	void testEquality_SameReference() {
+		Feet f1 = new Feet(1.0);
 
-        assertTrue(f1.equals(f1), "Object should be equal to itself (reflexive)");
-    }
+		assertTrue(f1.equals(f1), "Object should be equal to itself (reflexive)");
+	}
+
 }
