928deb602c8835b42ac968b073e181b1a3c619a4
anjaligitlab27 <anjalikumari270704@gmail.com>
2026-02-22 13:04:09 +0530

    [Anjali]: Added QuantityMeasurementApp class

--- src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
+++ src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
@@ -2,15 +2,31 @@
 package com.apps.quantitymeasurement;
 
 public class QuantityMeasurementApp {
-	public static void main(String[] args) {
 
-		// Feet equality check
-		Feet f1 = new Feet(1.0);
-		Feet f2 = new Feet(1.0);
+    // Static method for Feet equality
+    public static boolean areFeetEqual(double v1, double v2) {
+        Feet f1 = new Feet(v1);
+        Feet f2 = new Feet(v2);
+        return f1.equals(f2);
+    }
 
-		boolean result = f1.equals(f2);
-		System.out.println("Input: " + f1 + " and " + f2);
-		System.out.println("Output: Equal (" + result + ")");
+    // Static method for Inch equality
+    public static boolean areInchesEqual(double v1, double v2) {
+        Inch i1 = new Inch(v1);
+        Inch i2 = new Inch(v2);
+        return i1.equals(i2);
+    }
 
-	}
+    public static void main(String[] args) {
+
+        // Feet
+        boolean feetResult = areFeetEqual(1.0, 1.0);
+        System.out.println("Input: 1.0 ft and 1.0 ft");
+        System.out.println("Output: Equal (" + feetResult + ")");
+
+        // Inches
+        boolean inchResult = areInchesEqual(1.0, 1.0);
+        System.out.println("Input: 1.0 inch and 1.0 inch");
+        System.out.println("Output: Equal (" + inchResult + ")");
+    }
 }
