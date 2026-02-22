24d5c8e7e8e76ca46b6c7ae9a41344bd8fbb0914
anjaligitlab27 <anjalikumari270704@gmail.com>
2026-02-22 13:35:02 +0530

    [Anjali]: Added QuantityMeasurementApp class

--- src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
+++ src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
@@ -3,30 +3,18 @@ package com.apps.quantitymeasurement;
 
 public class QuantityMeasurementApp {
 
-    // Static method for Feet equality
-    public static boolean areFeetEqual(double v1, double v2) {
-        Feet f1 = new Feet(v1);
-        Feet f2 = new Feet(v2);
-        return f1.equals(f2);
-    }
+	public static void main(String[] args) {
 
-    // Static method for Inch equality
-    public static boolean areInchesEqual(double v1, double v2) {
-        Inch i1 = new Inch(v1);
-        Inch i2 = new Inch(v2);
-        return i1.equals(i2);
-    }
+		QuantityLength q1 = new QuantityLength(1.0, LengthUnit.FEET);
+		QuantityLength q2 = new QuantityLength(12.0, LengthUnit.INCH);
 
-    public static void main(String[] args) {
+		System.out.println("Input: " + q1 + " and " + q2);
+		System.out.println("Output: Equal (" + q1.equals(q2) + ")");
 
-        // Feet
-        boolean feetResult = areFeetEqual(1.0, 1.0);
-        System.out.println("Input: 1.0 ft and 1.0 ft");
-        System.out.println("Output: Equal (" + feetResult + ")");
+		QuantityLength q3 = new QuantityLength(1.0, LengthUnit.INCH);
+		QuantityLength q4 = new QuantityLength(1.0, LengthUnit.INCH);
 
-        // Inches
-        boolean inchResult = areInchesEqual(1.0, 1.0);
-        System.out.println("Input: 1.0 inch and 1.0 inch");
-        System.out.println("Output: Equal (" + inchResult + ")");
-    }
+		System.out.println("Input: " + q3 + " and " + q4);
+		System.out.println("Output: Equal (" + q3.equals(q4) + ")");
+	}
 }
