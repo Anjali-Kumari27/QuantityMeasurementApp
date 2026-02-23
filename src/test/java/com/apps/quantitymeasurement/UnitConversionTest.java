a9b9943f4e7161479c2f66155d3ac9d86386afe5
anjaligitlab27 <anjalikumari270704@gmail.com>
2026-02-23 14:38:59 +0530

    [Anjali] : Added modified LengthUnitTest file

--- src/test/java/com/apps/quantitymeasurement/UnitConversionTest.java
+++ src/test/java/com/apps/quantitymeasurement/UnitConversionTest.java
@@ -10,31 +10,31 @@ public class UnitConversionTest {
 
 	@Test
 	void testConversion_FeetToInches() {
-		double result = QuantityLength.convert(1.0, LengthUnit.FEET, LengthUnit.INCH);
+		double result = QuantityLength.convert(1.0, LengthUnit.FEET, LengthUnit.INCHES);
 		assertEquals(12.0, result, EPSILON);
 	}
 
 	@Test
 	void testConversion_InchesToFeet() {
-		double result = QuantityLength.convert(24.0, LengthUnit.INCH, LengthUnit.FEET);
+		double result = QuantityLength.convert(24.0, LengthUnit.INCHES, LengthUnit.FEET);
 		assertEquals(2.0, result, EPSILON);
 	}
 
 	@Test
 	void testConversion_YardsToInches() {
-		double result = QuantityLength.convert(1.0, LengthUnit.YARDS, LengthUnit.INCH);
+		double result = QuantityLength.convert(1.0, LengthUnit.YARDS, LengthUnit.INCHES);
 		assertEquals(36.0, result, EPSILON);
 	}
 
 	@Test
 	void testConversion_InchesToYards() {
-		double result = QuantityLength.convert(72.0, LengthUnit.INCH, LengthUnit.YARDS);
+		double result = QuantityLength.convert(72.0, LengthUnit.INCHES, LengthUnit.YARDS);
 		assertEquals(2.0, result, EPSILON);
 	}
 
 	@Test
 	void testConversion_CentimetersToInches() {
-		double result = QuantityLength.convert(2.54, LengthUnit.CENTIMETERS, LengthUnit.INCH);
+		double result = QuantityLength.convert(2.54, LengthUnit.CENTIMETERS, LengthUnit.INCHES);
 		assertEquals(1.0, result, EPSILON);
 	}
 
@@ -46,13 +46,13 @@ public class UnitConversionTest {
 
 	@Test
 	void testConversion_ZeroValue() {
-		double result = QuantityLength.convert(0.0, LengthUnit.FEET, LengthUnit.INCH);
+		double result = QuantityLength.convert(0.0, LengthUnit.FEET, LengthUnit.INCHES);
 		assertEquals(0.0, result, EPSILON);
 	}
 
 	@Test
 	void testConversion_NegativeValue() {
-		double result = QuantityLength.convert(-1.0, LengthUnit.FEET, LengthUnit.INCH);
+		double result = QuantityLength.convert(-1.0, LengthUnit.FEET, LengthUnit.INCHES);
 		assertEquals(-12.0, result, EPSILON);
 	}
 
@@ -60,8 +60,8 @@ public class UnitConversionTest {
 	void testConversion_RoundTrip_PreservesValue() {
 		double original = 5.0;
 
-		double converted = QuantityLength.convert(original, LengthUnit.FEET, LengthUnit.INCH);
-		double back = QuantityLength.convert(converted, LengthUnit.INCH, LengthUnit.FEET);
+		double converted = QuantityLength.convert(original, LengthUnit.FEET, LengthUnit.INCHES);
+		double back = QuantityLength.convert(converted, LengthUnit.INCHES, LengthUnit.FEET);
 
 		assertEquals(original, back, EPSILON);
 	}
@@ -80,9 +80,9 @@ public class UnitConversionTest {
 	@Test
 	void testConversion_NaNOrInfinite_Throws() {
 		assertThrows(IllegalArgumentException.class,
-				() -> QuantityLength.convert(Double.NaN, LengthUnit.FEET, LengthUnit.INCH));
+				() -> QuantityLength.convert(Double.NaN, LengthUnit.FEET, LengthUnit.INCHES));
 
 		assertThrows(IllegalArgumentException.class,
-				() -> QuantityLength.convert(Double.POSITIVE_INFINITY, LengthUnit.FEET, LengthUnit.INCH));
+				() -> QuantityLength.convert(Double.POSITIVE_INFINITY, LengthUnit.FEET, LengthUnit.INCHES));
 	}
 }
