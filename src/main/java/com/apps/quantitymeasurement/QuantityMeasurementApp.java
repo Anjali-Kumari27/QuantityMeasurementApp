1aa77b122afb3891e84a7f441adb0ee5d5a88187
anjaligitlab27 <anjalikumari270704@gmail.com>
2026-02-23 20:49:34 +0530

    [Anjali] : Added modified QuantityMeasurementApp class file

--- src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
+++ src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
@@ -92,6 +92,38 @@ public class QuantityMeasurementApp {
 		System.out.println("Volume Convert (1 gal -> L): " + v3.convertTo(VolumeUnit.LITRE));
 		System.out.println("Volume Add (1L + 1000mL -> L): " + v1.add(v2, VolumeUnit.LITRE));
 		System.out.println("Volume Add (1L + 1gal -> mL): " + v1.add(v3, VolumeUnit.MILLILITRE));
+
+		// UC12 Subtraction (Length)
+		// 9.50 feet -> prints 9.5 FEET due to rounding
+		System.out.println(l1 + " - " + l2 + " = " + l1.subtract(l2)); 
+		
+		// 114.0 INCHES
+		System.out.println(l1 + " - " + l2 + " in INCHES = " + l1.subtract(l2, LengthUnit.INCHES)); 
+
+		// UC12 Division (Length)
+		System.out.println(new Quantity<>(24.0, LengthUnit.INCHES).divide(new Quantity<>(2.0, LengthUnit.FEET))); // 1.0
+
+		// UC12 Subtraction (Weight)
+		// 5.0 KILOGRAM
+		System.out.println(w1 + " - " + w2 + " = " + w1.subtract(w2)); 
+		
+		// 5000.0 GRAM
+		System.out.println(w1 + " - " + w2 + " in GRAM = " + w1.subtract(w2, WeightUnit.GRAM)); 
+
+		// UC12 Division (Weight)
+		System.out.println(new Quantity<>(2000.0, WeightUnit.GRAM).divide(new Quantity<>(1.0, WeightUnit.KILOGRAM))); 
+
+		// UC12 Subtraction (Volume)
+		// 4.5 LITRE
+		System.out.println(v1 + " - " + v2 + " = " + v1.subtract(v2)); 
+		
+		// 3000.0 MILLILITRE
+		System.out.println(v1 + " - " + new Quantity<>(2.0, VolumeUnit.LITRE) + " in MILLILITRE = "
+				+ v1.subtract(new Quantity<>(2.0, VolumeUnit.LITRE), VolumeUnit.MILLILITRE)); 
+
+		// UC12 Division (Volume)
+		 // 0.5
+		System.out.println(new Quantity<>(5.0, VolumeUnit.LITRE).divide(new Quantity<>(10.0, VolumeUnit.LITRE)));
 	}
 
 }
