ba10d0f62ca0372dcd47fa271b6b41dd0a6aeac4
anjaligitlab27 <anjalikumari270704@gmail.com>
2026-03-24 16:11:47 +0530

    [Anjali] : Added Repository Layer

---
+++ src/main/java/com/app/quantitymeasurement/repository/QuantityMeasurementRepository.java
@@ -0,0 +1,39 @@
+package com.app.quantitymeasurement.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.app.quantitymeasurement.model.QuantityMeasurementEntity;
+
+/**
+ * Repository interface for QuantityMeasurementEntity
+ *
+ * Spring Data JPA creates implementation automatically.
+ */
+@Repository
+public interface QuantityMeasurementRepository extends JpaRepository<QuantityMeasurementEntity, Long> {
+
+	/**
+	 * Get history by operation
+	 */
+	List<QuantityMeasurementEntity> findByOperation(String operation);
+
+	/**
+	 * Get history by measurement type
+	 */
+	List<QuantityMeasurementEntity> findByThisMeasurementType(String measurementType);
+
+	/**
+	 * Count only successful operations
+	 *
+	 * Works only if entity field name is 'error'
+	 */
+	long countByOperationAndErrorFalse(String operation);
+
+	/**
+	 * Get only failed operations
+	 */
+	List<QuantityMeasurementEntity> findByErrorTrue();
+}
