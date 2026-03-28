ad2541074722e8f26e86fa932f9e0c2966c29b89
anjaligitlab27 <anjalikumari270704@gmail.com>
2026-03-28 12:39:45 +0530

    [Anjali] : Added Authentication DTO files

---
+++ src/main/java/com/app/quantitymeasurement/auth/AuthRequest.java
@@ -0,0 +1,20 @@
+package com.app.quantitymeasurement.auth;
+
+/*
+ * AuthRequest DTO
+ *
+ * This class is used to receive login request data from client.
+ *
+ * It contains:
+ * - email: user's login email
+ * - password: user's password
+ *
+ * This data is sent from frontend/Postman to /login API.
+ */
+import lombok.Data;
+
+@Data
+public class AuthRequest {
+	private String email;
+	private String password;
+}
---
+++ src/main/java/com/app/quantitymeasurement/auth/RegisterRequest.java
@@ -0,0 +1,22 @@
+package com.app.quantitymeasurement.auth;
+
+/*
+ * RegisterRequest DTO
+ *
+ * This class is used to receive user registration data.
+ *
+ * It contains:
+ * - fullName: user's name
+ * - email: user's unique email
+ * - password: user's password
+ *
+ * This data is used to create a new user in database.
+ */
+import lombok.Data;
+
+@Data
+public class RegisterRequest {
+	private String fullName;
+	private String email;
+	private String password;
+}
