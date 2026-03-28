0bbdb1bb0dde7fd4d0d540ac3447d21a980e68ea
anjaligitlab27 <anjalikumari270704@gmail.com>
2026-03-28 12:37:09 +0530

    [Anjali] : Added JwtService file

---
+++ src/main/java/com/app/quantitymeasurement/service/JwtService.java
@@ -0,0 +1,63 @@
+package com.app.quantitymeasurement.service;
+
+/*
+ * JWT Service Class
+ *
+ * This class handles all JWT operations.
+ *
+ * Responsibilities:
+ * 1. Generate token when user logs in
+ * 2. Extract username from token
+ * 3. Validate token (check expiration and correctness)
+ *
+ * JWT contains:
+ * - Subject (username/email)
+ * - Issued time
+ * - Expiry time
+ */
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+@Service
+public class JwtService {
+
+	@Value("${jwt.secret}")
+	private String secretKey;
+
+	@Value("${jwt.expiration}")
+	private long jwtExpiration;
+
+	private SecretKey getSignInKey() {
+		return Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+	}
+
+	public String generateToken(UserDetails userDetails) {
+		return Jwts.builder().subject(userDetails.getUsername()).issuedAt(new Date())
+				.expiration(new Date(System.currentTimeMillis() + jwtExpiration)).signWith(getSignInKey()).compact();
+	}
+
+	public String extractUsername(String token) {
+		return Jwts.parser().verifyWith(getSignInKey()).build().parseSignedClaims(token).getPayload().getSubject();
+	}
+
+	public boolean isTokenValid(String token, UserDetails userDetails) {
+		final String username = extractUsername(token);
+		return username.equals(userDetails.getUsername()) && !isTokenExpired(token);
+	}
+
+	private boolean isTokenExpired(String token) {
+		Date expiration = Jwts.parser().verifyWith(getSignInKey()).build().parseSignedClaims(token).getPayload()
+				.getExpiration();
+
+		return expiration.before(new Date());
+	}
+}
